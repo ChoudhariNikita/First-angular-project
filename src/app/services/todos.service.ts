e2ab2751759080d8767ac866f03f42a79ba3bb4d
ChoudhariNikita <choudharinikita96@gmail.com>
2025-01-07 11:55:39 +0530

    Done with making http calls

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,24 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import {Todo} from '../model/todo.type'
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root' // service available to all components
 })
 export class TodosService {
-  todoItems:Array<Todo>=[
-    {
-      id:0,
-      title:"Learn Angular",
-      userId:1,
-      completed:false
-    },
-    {
-      id:1,
-      title:"Learn AngularJS",
-      userId:1,
-      completed:false
-    }
-  ]
+  http=inject(HttpClient);
+  // todoItems:Array<Todo>=[
+  //   {
+  //     id:0,
+  //     title:"Learn Angular",
+  //     userId:1,
+  //     completed:false
+  //   },
+  //   {
+  //     id:1,
+  //     title:"Learn AngularJS",
+  //     userId:1,
+  //     completed:false
+  //   }
+  // ]
   
-  constructor() { }
+  getTodosFromApi(){
+    const url=`https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get<Array<Todo>>(url);
+  }
+  
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component,inject,OnInit,signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -20,10 +21,18 @@ export class TodosComponent implements OnInit {
   // this method is called when the component is initialized
   ngOnInit():void{
     // log the list of todos to the console
-    console.log(this.todoService.todoItems);
+    // console.log(this.todoService.todoItems);
 
     // set the value of the todoItems signal to the list of todos stored in the service
-    this.todoItems.set(this.todoService.todoItems);
+    // this.todoItems.set(this.todoService.todoItems);
+
+    // get data from api
+    this.todoService.getTodosFromApi()
+    .pipe( 
+      catchError((error)=>{console.log(error);return []})
+    ).subscribe((todos)=>{
+      this.todoItems.set(todos);
+    });
   }
 }
 
